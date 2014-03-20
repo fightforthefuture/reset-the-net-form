d406383739637b739cdd3a5598ae6eb157e8adaa
Chris Antaki <ChrisAntaki@gmail.com>
2014-03-19 18:35:44 -0700

    Readying form

--- js/test.js
+++ js/test.js
@@ -2,9 +2,9 @@ $('.help').popover();
 $('form').on('submit', function(e) {
     $('form').validate();
 
-    var res = $('form').valid();
+    var valid = $('form').valid();
 
-    console.log(res); // TODO: Remove this debug code.
-
-    e.preventDefault();
+    if (!valid) {
+        e.preventDefault();
+    }
 });
