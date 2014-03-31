bd94dfd80a2d0886912c9d2e027aa3ab6af9d597
Chris Antaki <ChrisAntaki@gmail.com>
2014-03-31 13:52:58 -0700

    The image is now required

--- js/form.js
+++ js/form.js
@@ -2,16 +2,46 @@
 
 
 
+// Shortcuts.
+var file = false
+var previewImg = $('#preview img')[0];
+
+
+
+// Setup popover.
+var popoverTimeout = false;
+$(previewImg).popover({
+    content: 'Please upload a beautiful image!',
+    placement: 'bottom',
+    trigger: 'manual'
+});
+
+
+
 // Validate Form.
 $('form').on('submit', function(e) {
     $('form').validate();
 
     var valid = $('form').valid();
 
+    // Image is missing.
+    if (!file) {
+        valid = false;
+
+        $(previewImg).popover('show');
+
+        clearTimeout(popoverTimeout);
+        popoverTimeout = setTimeout(function() {
+            $(previewImg).popover('hide');
+        }, 3000);
+    }
+
     if (!valid) {
         e.preventDefault();
     }
 });
+
+
 
 // Update Form destination.
 if (location.host === 'localhost:8080') {
@@ -20,19 +50,21 @@ if (location.host === 'localhost:8080') {
     });
 }
 
+
+
 // Show image preview.
 $('#image').on('change', showPreview);
 function showPreview() {
     if (this.files && this.files[0]) {
-        var file = this.files[0];
-        var previewImg = $('#preview img')[0];
+        file = this.files[0];
         var reader = new FileReader();
 
         reader.onloadend = function() {
             if (file.type === 'image/png' || file.type === 'image/jpeg') {
                 previewImg.src = reader.result;
             } else {
-                previewImg.src = '';
+                previewImg.src = 'images/upload.png';
+                file = false;
             }
 
             console.log('File detected!!'); // TODO: Remove this debug code.
